41f83646be95c131b8f0456e7405fa9553cc4a7a
rwiteshbera <rwiteshbera@gmail.com>
2022-05-06 03:33:29 +0530

    Minor changes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ColorModeScript } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ColorModeScript />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
