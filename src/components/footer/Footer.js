76b680cc86e0205af0bddbb53f53f091728fd969
rwiteshbera <rwiteshbera@gmail.com>
2022-05-07 21:05:19 +0530

    Added counter

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,30 @@
-import { Box, Center } from '@chakra-ui/react'
-import React from 'react'
-import "./Footer.css"
+import { Box, Center } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './Footer.css';
 
 const Footer = () => {
-  return (
-    <div className='footer'>
-        <Center pt={["2","2","3","3"]}>&copy; Rwitesh Bera | 2022</Center>
-    </div>
-  )
-}
+  const [pageVisited, setPageVisited] = useState(0);
 
-export default Footer
+  const getCounter = async () => {
+    await axios
+      .get(
+        `https://api.countapi.xyz/hit/rwiteshbera.com/${process.env.COUNTAPI_KEY}`
+      )
+      .then(res => {
+        setPageVisited(res.data.value);
+      });
+  };
+
+  useEffect(() => {
+    getCounter();
+  }, []);
+
+  return (
+    <div className="footer">
+      <Center pt={['2', '2', '3', '3']}>&copy; Rwitesh Bera | 2022 | Page Visited : {pageVisited}</Center>
+    </div>
+  );
+};
+
+export default Footer;
