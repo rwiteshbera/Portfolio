a0ffdce113f23fcc1e0033310437296513aa6b87
rwiteshbera <rwiteshbera@gmail.com>
2022-05-08 21:04:11 +0530

    Removed count api

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,28 +1,12 @@
 import { Box, Center } from '@chakra-ui/react';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  const [pageVisited, setPageVisited] = useState(0);
-
-  const getCounter = async () => {
-    await axios
-      .get(
-        `https://api.countapi.xyz/hit/rwiteshbera.com/${process.env.COUNTAPI_KEY}`
-      )
-      .then(res => {
-        setPageVisited(res.data.value);
-      });
-  };
-
-  useEffect(() => {
-    getCounter();
-  }, []);
-
   return (
     <div className="footer">
-      <Center pt={['2', '2', '3', '3']}>&copy; Rwitesh Bera | 2022 | Page Visited : {pageVisited}</Center>
+      <Center pt={['2', '2', '3', '3']}>&copy; Rwitesh Bera | 2022</Center>
     </div>
   );
 };
