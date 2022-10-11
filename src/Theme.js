c6be818e1e95e588a351a54f10c8ae1ba03e7d9b
Rwitesh Bera <rwiteshbera@gmail.com>
2022-10-12 00:30:52 +0530

    Update Theme.js

--- src/Theme.js
+++ src/Theme.js
@@ -1,13 +1,13 @@
-import { extendTheme } from "@chakra-ui/react"
+// 1. import `extendTheme` function
+import { extendTheme } from '@chakra-ui/react'
 
-export const theme = extendTheme({
-  colors: {
-    brand: {
-      100: "rgb(28,28,28)",
-      200: "white",
-      300: "#0f0e13",
-    },
-  },
-  
-})
+// 2. Add your color mode config
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
 
+// 3. extend the theme
+const theme = extendTheme({ config })
+
+export default theme
